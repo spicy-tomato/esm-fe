eb6d534cf3b219bb2f5e55ed7ef2529265cea61e
An Nguyen <snowflower1408@gmail.com>
2023-02-05 19:12:47 +0700

    chore: initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TuiRootModule } from '@taiga-ui/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        TuiRootModule,
         RouterTestingModule
       ],
       declarations: [
@@ -24,12 +26,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('esm-fe');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('esm-fe app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
+import { inject, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TUI_IS_CYPRESS } from '@taiga-ui/cdk';
+import {
+  TuiAlertModule,
+  TuiDialogModule,
+  TuiRootModule,
+  TUI_ANIMATIONS_DURATION,
+} from '@taiga-ui/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const TAIGA_UI = [TuiRootModule, TuiAlertModule, TuiDialogModule];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ...TAIGA_UI,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  providers: [
+    {
+      provide: TUI_ANIMATIONS_DURATION,
+      useFactory: () => (inject(TUI_IS_CYPRESS) ? 0 : 300),
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
