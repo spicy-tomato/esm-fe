f9b48dfb11df9e4f8c12711032aa5c5eff03b58d
An Nguyen <annguyen.it.utc@gmail.com>
2023-05-20 17:39:06 +0700

    ci: fix sonar

--- karma.conf.js
+++ karma.conf.js
@@ -13,21 +13,19 @@ module.exports = function (config) {
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
+      jasmine: {},
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/esm-fe"),
+      dir: require("path").join(__dirname, "./coverage"),
       subdir: ".",
-      reporters: [{ type: "lcov" }],
+      reporters: [
+        { type: "html", subdir: "html-report" },
+        { type: "lcov", subdir: "lcov-report" },
+      ],
     },
     reporters: ["progress", "kjhtml"],
     port: 9876,
