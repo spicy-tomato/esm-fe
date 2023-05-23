14f471e9e78ba21cd701b4c47211def22fdae200
An Nguyen <annguyen.it.utc@gmail.com>
2023-05-23 18:58:59 +0700

    chore: hide unfinished functions

--- src/app/shared/components/layout/side-bar/side-bar.constant.ts
+++ src/app/shared/components/layout/side-bar/side-bar.constant.ts
@@ -58,55 +58,55 @@ export class SideBarConstant {
       routerLink: '/exam/handover',
       roles: [Role.EXAMINATION_DEPARTMENT_HEAD],
     },
-    {
-      name: 'Thanh toán',
-      icon: 'far fa-wallet',
-      roles: [Role.EXAMINATION_DEPARTMENT_HEAD],
-      subItems: [
-        {
-          name: 'Ban coi thi',
-          routerLink: '/paid/invigilator-department',
-          icon: 'far fa-money-bill',
-        },
-        {
-          name: 'Ban đề thi',
-          routerLink: '/paid/exam-department',
-          icon: 'far fa-money-bill',
-        },
-        {
-          name: 'Cán bộ coi thi',
-          routerLink: '/paid/invigilator',
-          icon: 'far fa-money-bill',
-        },
-      ],
-    },
-    {
-      name: 'Báo cáo, thống kê',
-      roles: [Role.EXAMINATION_DEPARTMENT_HEAD],
-      icon: 'far fa-chart-pie',
-      subItems: [
-        {
-          name: 'Tiến độ',
-          routerLink: '/process',
-          icon: 'far fa-tasks',
-        },
-        {
-          name: 'Tổng hợp',
-          routerLink: '/',
-          icon: 'far fa-clipboard-list',
-        },
-        {
-          name: 'Báo cáo',
-          routerLink: '/report',
-          icon: 'far fa-file-chart-pie',
-        },
-      ],
-    },
-    {
-      name: 'Thông báo, tài liệu',
-      icon: 'far fa-folders',
-      routerLink: '/document',
-    },
+    // {
+    //   name: 'Thanh toán',
+    //   icon: 'far fa-wallet',
+    //   roles: [Role.EXAMINATION_DEPARTMENT_HEAD],
+    //   subItems: [
+    //     {
+    //       name: 'Ban coi thi',
+    //       routerLink: '/paid/invigilator-department',
+    //       icon: 'far fa-money-bill',
+    //     },
+    //     {
+    //       name: 'Ban đề thi',
+    //       routerLink: '/paid/exam-department',
+    //       icon: 'far fa-money-bill',
+    //     },
+    //     {
+    //       name: 'Cán bộ coi thi',
+    //       routerLink: '/paid/invigilator',
+    //       icon: 'far fa-money-bill',
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: 'Báo cáo, thống kê',
+    //   roles: [Role.EXAMINATION_DEPARTMENT_HEAD],
+    //   icon: 'far fa-chart-pie',
+    //   subItems: [
+    //     {
+    //       name: 'Tiến độ',
+    //       routerLink: '/process',
+    //       icon: 'far fa-tasks',
+    //     },
+    //     {
+    //       name: 'Tổng hợp',
+    //       routerLink: '/',
+    //       icon: 'far fa-clipboard-list',
+    //     },
+    //     {
+    //       name: 'Báo cáo',
+    //       routerLink: '/report',
+    //       icon: 'far fa-file-chart-pie',
+    //     },
+    //   ],
+    // },
+    // {
+    //   name: 'Thông báo, tài liệu',
+    //   icon: 'far fa-folders',
+    //   routerLink: '/document',
+    // },
     {
       name: 'Quản lý kỳ thi',
       roles: [Role.EXAMINATION_DEPARTMENT_HEAD],
